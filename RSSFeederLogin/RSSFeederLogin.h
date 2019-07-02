540eb271be6c208587055f4218617e1f4bf043b6
Santhosh <santhosh_nampally@epam.com>
2019-07-02 12:10:32 +0530

    Initial Commit

---
+++ RSSFeederLogin/RSSFeederLogin.h
@@ -0,0 +1,19 @@
+//
+//  RSSFeederLogin.h
+//  RSSFeederLogin
+//
+//  Created by Santhosh Nampally on 02/07/19.
+//  Copyright © 2019 Santhosh Nampally. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RSSFeederLogin.
+FOUNDATION_EXPORT double RSSFeederLoginVersionNumber;
+
+//! Project version string for RSSFeederLogin.
+FOUNDATION_EXPORT const unsigned char RSSFeederLoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSSFeederLogin/PublicHeader.h>
+
+
